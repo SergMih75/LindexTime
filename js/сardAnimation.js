bad719ffd0124192513b2ee880711479af9e3aa6
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2025-03-27 22:05:10 +0300

    add programs section + card move js fix

--- js/сardAnimation.js
+++ js/сardAnimation.js
@@ -1,9 +1,11 @@
 const techCard = document.querySelectorAll('.card')
 const speakerCard = document.querySelectorAll('.speaker__item')
+const programCard = document.querySelectorAll('.program__item')
 
 document.addEventListener('scroll', () => {
 	techCardMove(techCard)
     techCardMove(speakerCard)
+	techCardMove(programCard)
 	// console.log(window.innerHeight);
 	// console.log(item);
 	// console.log(item.clientHeight);
@@ -12,16 +14,23 @@ document.addEventListener('scroll', () => {
 
 function techCardMove(card) {
 	card.forEach(item => {
+		let temp = item.getAttribute('data-move')
+
 		if (
-			item.getBoundingClientRect().top + item.clientHeight / 2 <
+			item.getBoundingClientRect().top + item.clientHeight / 4 <
 			window.innerHeight
 		) {
 			item.style.transform = 'translateX(0)'
-			item.style.transition = '900ms'
+			item.style.transition = '1200ms'
 		}
 
 		if (item.getBoundingClientRect().top > window.innerHeight) {
-			item.style.transform = 'translateX(-100vw)'
+			if (temp === 'left') {
+				item.style.transform = 'translateX(-100vw)'
+			}
+			if (temp === 'right') {
+				item.style.transform = 'translateX(150vw)'
+			}
 			item.style.transition = '900ms'
 		}
 	})
